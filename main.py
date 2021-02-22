deaac2e910bea350c8a6060e958cecedb6364aa1
Adrian <adriansam.pl3@gmail.com>
2021-02-22 14:23:05 +0100

    zad.2

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-# zad1
-a1= 'napis'
-a2= 'napis2'
-b1= 3
-b2= 4
-c1= 2.5
-c2= 3.9
-d1= 5+4j
-d2= 3-2j
-print(type(a1))
-print(type(b1))
-print(type(c1))
-print(type(d1))
+# zad2
+a= 4
+b= 10
+c= 2
+dodawanie= a+b
+odejmowanie= a-b
+mnozenie= a*b
+dzielenie= b/c
+dzielenie_calk=b//a
+print(dodawanie)
+print(odejmowanie)
+print(mnozenie)
+print(dzielenie)
+print(dzielenie_calk)
+
