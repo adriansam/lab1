07222ea3c2a02cdea5379df6d05f176102aef61a
Adrian <adriansam.pl3@gmail.com>
2021-03-01 00:30:25 +0100

    zadania

--- main.py
+++ main.py
@@ -1,29 +1,87 @@
 # zad1
-a1= 'napis'
-a2= 'napis2'
-b1= 3
-b2= 4
-c1= 2.5
-c2= 3.9
-d1= 5+4j
-d2= 3-2j
-print(type(a1))
-print(type(b1))
-print(type(c1))
-print(type(d1))
+# a1= 'napis'
+# a2= 'napis2'
+# b1= 3
+# b2= 4
+# c1= 2.5
+# c2= 3.9
+# d1= 5+4j
+# d2= 3-2j
+# print(type(a1))
+# print(type(b1))
+# print(type(c1))
+# print(type(d1))
 
 # zad2
-a= 4
-b= 10
-c= 2
-dodawanie= a+b
-odejmowanie= a-b
-mnozenie= a*b
-dzielenie= b/c
-dzielenie_calk=b//a
-print(dodawanie)
-print(odejmowanie)
-print(mnozenie)
-print(dzielenie)
-print(dzielenie_calk)
+# a= 4
+# b= 10
+# c= 2
+# dodawanie= a+b
+# odejmowanie= a-b
+# mnozenie= a*b
+# dzielenie= b/c
+# dzielenie_calk=b//a
+# print(dodawanie)
+# print(odejmowanie)
+# print(mnozenie)
+# print(dzielenie)
+# print(dzielenie_calk)
 
+# zad3
+# a = 2
+# b = 4
+# c = 2
+# d = 2
+# e = 2
+# f = 2
+# g = 2
+# a+=b
+# print(a)
+# c-=b
+# print(c)
+# d*=b
+# print(d)
+# e/=b
+# print(e)
+# f**=b
+# print(f)
+# g%=b
+# print(g)
+
+# zad4
+# from math import *
+# e = 2
+# e**=10
+# print(e)
+# print(log(5+sin(radians(8)**2)**(1/6)))
+# print(math.ceil(4.80))
+# print(math.floor(3.55))
+
+# zad5
+# imie,nazwisko="ADRIAN", "SIEMASZKO"
+# print(imie.capitalize())
+# print(nazwisko.capitalize())
+
+# zad6
+# string = "la la la la la"
+# x=string.count("la")
+# print(x)
+
+# zad7
+# i="fenyloetyloamina"
+# print(i[0])
+# print(i[len(i)-1])
+
+#zad8
+# string = "la la la la la"
+# print(string.split())
+
+#zad9
+from math import *
+# a = "programowanie"
+# b = 4.2
+# c = 10
+# c = hex(c)
+# print(a)
+# print(b)
+# print(c)
